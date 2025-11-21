6888993dc1ee816556b5cf906eed5128fd2db745
1026hz <1026hzz@gmail.com>
2025-11-21 23:05:58 +0900

    fix: CORS config

--- src/main/java/com/panda/backend/global/config/CorsConfig.java
+++ src/main/java/com/panda/backend/global/config/CorsConfig.java
@@ -7,13 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    private static final String[] ALLOWED_ORIGINS = {
+        "http://localhost:3000",
+        "http://localhost:8000",
+        "http://3.34.216.85",
+        "https://3.34.216.85"
+    };
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://3.34.216.85", "https://3.34.216.85")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+            .allowedOrigins(ALLOWED_ORIGINS)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
 }
